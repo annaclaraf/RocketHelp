c680ed5af92ba8e6d0689cb15e51d8fec912754c
Anna <anisferraz@gmail.com>
2022-08-31 19:15:03 -0300

    add routes

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { THEME } from './src/styles/theme'
 
 import { Loading } from './src/components/Loading'
-import { SignIn } from './src/screens/SignIn'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold })
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME} >
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      { fontsLoaded ? <SignIn /> : <Loading/> }
+      { fontsLoaded ? <Routes /> : <Loading/> }
     </NativeBaseProvider>
   );
 }
